255dabd9ffd19ba89db9c216abcbc2c87d3ff9e3
EnderMandS <952025764@qq.com>
2023-11-09 21:19:14 +0800

    20231103 official update

--- run_docker_simulator.sh
+++ run_docker_simulator.sh
@@ -1,1 +1,1 @@
-docker run -it -e RenderOffScreen=1  -e Seed=$1 --rm --name sim02 --net host --gpus 'device=0' simulator02
+docker run -it -e RenderOffScreen=1 -e Seed=$1 --name rm_sim --net host --gpus 'device=0' simulator02
